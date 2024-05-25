00763b9bd70fdf48a4bf8bc3998c936b7764a6d1
jumir gosain <jumir1414@gmail.com>
2024-05-25 21:56:34 +0545

    feat: :sparkles: adding errror handling and error middleware creation

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,34 +1,67 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "../routes";
 import { hashSync, compareSync } from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../secrets";
+import { BadRequestsException } from "../exceptions/bad-requests";
+import { HttpStatusErrorCode } from "../exceptions/root";
 
-export const signUp = async (req: Request, res: Response) => {
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, password, name } = req.body;
-  let user = await prismaClient.user.findFirst({ where: { email } });
-  if (user) {
-    throw Error("User already Exits");
-  }
-  user = await prismaClient.user.create({
-    data: {
-      name,
-      email,
-      password: hashSync(password, 10),
-    },
-  });
-  res.json(user);
-};
 
-export const login = async (req: Request, res: Response) => {
+  try {
+    // Check if the user already exists
+    let user = await prismaClient.user.findFirst({ where: { email } });
+    if (user) {
+      return next(
+        new BadRequestsException(
+          "User already exists",
+          HttpStatusErrorCode.FORBIDDEN
+        )
+      );
+    }
+
+    // Create new user
+    user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password: hashSync(password, 10),
+      },
+    });
+
+    res.json(user);
+  } catch (error) {
+    // Pass any error to the next middleware
+    next(error);
+  }
+};
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { email, password } = req.body;
   let user = await prismaClient.user.findFirst({ where: { email } });
   if (!user) {
-    res.status(404).send("user not found");
-    throw Error("User not found");
+    return next(
+      new BadRequestsException(
+        "User not found",
+        HttpStatusErrorCode.BAD_REQUEST
+      )
+    );
   }
   if (!compareSync(password, user?.password)) {
-    throw Error("Incorrect Password!!");
+    return next(
+      new BadRequestsException(
+        "Incorrect password",
+        HttpStatusErrorCode.FORBIDDEN
+      )
+    );
   }
   const token = jwt.sign(
     {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
 app.use(express.json());
 app.use("/api", rootRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log("app working");
